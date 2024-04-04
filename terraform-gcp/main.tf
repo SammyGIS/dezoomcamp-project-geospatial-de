ab0d3d5b9ed69c48b60efe2be6b9d53d0483ffec
SammyGIS <adedoyinsamuel25@gmail.com>
2024-04-04 11:34:18 +0100

    add ndmi Cf to main.tf

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -90,7 +90,7 @@ resource "google_cloudfunctions_function" "gee_ndvi_function" {
   source_archive_bucket = google_storage_bucket.function_bucket.name
   source_archive_object = google_storage_bucket_object.zipped_code.name
 
-  available_memory_mb = 8192
+  available_memory_mb = 4098
 
   trigger_http = true
   entry_point  = "main"
@@ -109,6 +109,58 @@ resource "google_cloud_scheduler_job" "automate_ndvi" {
   http_target {
     http_method = "GET"
     uri         = google_cloudfunctions_function.gee_ndvi_function.https_trigger_url
+    oidc_token {
+      service_account_email = var.service_account_email
+    }
+  }
+}
+
+
+
+data "archive_file" "ndmi_source" {
+  type        = "zip"
+  source_dir  = "${path.module}/ndmi_function"
+  output_path = "${path.module}/ndmi_function.zip"
+}
+
+resource "google_storage_bucket_object" "zipped_code" {
+  source       = data.archive_file.ndmi_source.output_path
+  content_type = "application/zip"
+
+  name   = "src-${data.archive_file.ndmi_source.output_md5}.zip"
+  bucket = google_storage_bucket.function_bucket.name
+}
+
+resource "google_cloudfunctions_function" "gee_ndmi_function" {
+  name     = "gee_ndmi_function"
+  runtime = "python39"
+  region   = var.region_function
+  
+  # Set the timeout duration here
+  timeout = 300  # 5 minutes
+
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.zipped_code.name
+
+  available_memory_mb = 4098
+
+  trigger_http = true
+  entry_point  = "main"
+  environment_variables = {
+    # Use a more descriptive variable name here
+    deployment_name = "terraform"
+  }
+}
+
+resource "google_cloud_scheduler_job" "automate_ndmi" {
+  name               = "gee_ndmi_function"
+  description        = "get ndmi data every 7 days"
+  region              = var.region_function
+  schedule           = "0/2 * * * *"
+  time_zone = "Africa/Lagos"
+  http_target {
+    http_method = "GET"
+    uri         = google_cloudfunctions_function.gee_ndmi_function.https_trigger_url
     oidc_token {
       service_account_email = var.service_account_email
     }
